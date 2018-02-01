46e71999c94c4c016146bdf1d36a91c0a87ade40
William Amato <IamWill216@yahoo.com>
2018-02-01 18:37:53 -0500

    i have stuff on my other computer..ha

---
+++ friendFinder.js
@@ -0,0 +1,6 @@
+//create an express server
+//create objects that store "scores" of certain fields
+//create a way to compare the scores to the input fields of html a user inputs
+//create a best match system to then display what the user matches with 
+//profit
+//also do the assignment and not leave it on your other computer...yeah that part is important.
